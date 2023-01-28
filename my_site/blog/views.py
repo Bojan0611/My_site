3421d76e564cf40d3bc852c4b79ab55c7681a458
Bojan0611 <jbojanowski11@gmail.com>
2023-01-28 23:29:07 +0100

    add first template and base template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def starting_page(request):
-    return HttpResponse("It works!")
+    return render(request, "blog/index.html")
 
 def posts(request):
     pass
