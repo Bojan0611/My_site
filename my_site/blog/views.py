bc783caecb635b68ff968ad1bab7dd7d08272c95
Bojan0611 <jbojanowski11@gmail.com>
2023-01-29 22:29:59 +0100

    add new styling for all posts page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 
 def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 def post_details(request):
     pass
