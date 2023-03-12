1dce027ecb78bf404c9480a10ff7ede314a3e7e5
Bojan0611 <jbojanowski11@gmail.com>
2023-03-12 14:15:53 +0100

    add Author, Tag and Post models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    
+class Tag(models.Model):
+    caption = models.CharField()
+    
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField()
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    
