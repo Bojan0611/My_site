f31548a55c5667e9d60b321bf048a4b0aca3c0c4
Bojan0611 <jbojanowski11@gmail.com>
2023-03-12 23:45:53 +0100

    new views - based on database data

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Author, Tag, Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("title", )
+    }
+    list_display = ("title", "date", "author",)
+    list_filter = ("author", "date", "tag",)
+
 
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -6,8 +6,18 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+    
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.caption
+    
     
 class Post(models.Model):
     author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="posts")
@@ -18,4 +28,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    
+    prepopulated_field = {
+        "slug": ("title",)
+    }
     
