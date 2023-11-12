b261338e16162a340c39fd16f71c94966da661cd
Bojan95 <jbojanowski11@gmail.com>
2023-11-12 17:27:29 +0100

    change post model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag)
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=250)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
